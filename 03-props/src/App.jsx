e5bb681eb17f74ede3643bef92583cff1f232875
Jackal <mofizusmani1@gmail.com>
2025-12-30 18:44:44 +0530

    props

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -4,7 +4,10 @@ const App = () => {
   return (
     <div className='parent'>
       <div className="card">
+        <img src='https://images.unsplash.com/photo-1756137074695-88e71b00ef77?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
         <h1>Hello Mike Ross</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem quos odio ad eius laudantium, beatae facilis dolorem tenetur est aliquam. Esse in earum quo porro doloribus sunt, ad harum obcaecati!</p>
+        <button>View Profile</button>
       </div>
     </div>
   )
